77a4d782cdb85cf05246023405a90bd8dc727476
Gustavo Marques Reis <62080947+GustavoMarques02@users.noreply.github.com>
2023-02-07 21:16:09 -0300

    Update HomeActivity.java

--- app/src/main/java/com/example/thermoledmobileclient/HomeActivity.java
+++ app/src/main/java/com/example/thermoledmobileclient/HomeActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -23,8 +24,10 @@ import io.grpc.examples.iotservice.LedRequest;
 
 public class HomeActivity extends AppCompatActivity {
     private Button temperatureButton;
-    private Button ledOnButton;
-    private Button ledOffButton;
+    private Button ledOnButtonRed;
+    private Button ledOffButtonRed;
+    private Button ledOnButtonGreen;
+    private Button ledOffButtonGreen;
     private String host, port, userId;
 
     @Override
@@ -39,8 +42,10 @@ public class HomeActivity extends AppCompatActivity {
         this.userId = message[2];
 
         temperatureButton = findViewById(R.id.temperatureButton);
-        ledOnButton = findViewById(R.id.ledOnButton);
-        ledOffButton = findViewById(R.id.ledOffButton);
+        ledOnButtonRed = findViewById(R.id.ledOnButtonRed);
+        ledOffButtonRed = findViewById(R.id.ledOffButtonrRed);
+        ledOnButtonGreen = findViewById(R.id.ledOnButtonrGreen);
+        ledOffButtonGreen = findViewById(R.id.ledOffButtonrGreen);
     }
 
     public void getTemperature(View view) {
@@ -50,14 +55,24 @@ public class HomeActivity extends AppCompatActivity {
         this.startActivity(intent);
     }
 
-    public void ledOnRequest(View view) {
-        ledOnButton.setEnabled(false);
-        new GrpcTask2(this).execute("", "", "1");
+    public void ledOnRequestRed(View view) {
+        ledOnButtonRed.setEnabled(false);
+        new GrpcTask2(this).execute(this.host, this.port, "led1", this.userId, "1");
     }
 
-    public void ledOffRequest(View view) {
-        ledOffButton.setEnabled(false);
-        new GrpcTask2(this).execute("", "", "0");
+    public void ledOffRequestRed(View view) {
+        ledOffButtonRed.setEnabled(false);
+        new GrpcTask2(this).execute(this.host, this.port, "led1", this.userId, "0");
+    }
+
+    public void ledOnRequestGreen(View view) {
+        ledOnButtonRed.setEnabled(false);
+        new GrpcTask2(this).execute(this.host, this.port, "led2", this.userId, "1");
+    }
+
+    public void ledOffRequestGreen(View view) {
+        ledOffButtonRed.setEnabled(false);
+        new GrpcTask2(this).execute(this.host, this.port, "led2", this.userId, "0");
     }
 
     private static class GrpcTask2 extends AsyncTask<String, Void, String> {
@@ -74,14 +89,15 @@ public class HomeActivity extends AppCompatActivity {
 
             String host = params[0];
             String portStr = params[1];
-            String userId = params[2];
-            int ledState = Integer.parseInt(params[3]);
+            String ledId = params[2];
+            String userId = params[3];
+            int ledState = Integer.parseInt(params[4]);
             int port = TextUtils.isEmpty(portStr) ? 0 : Integer.parseInt(portStr);
             try {
                 channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
                 IoTServiceGrpc.IoTServiceBlockingStub stub = IoTServiceGrpc.newBlockingStub(channel);
                 LedRequest request = LedRequest.newBuilder().setState(ledState)
-                        .setUserId(userId).build();
+                        .setUserId(userId).setLedId(ledId).build();
                 LedReply reply = stub.blinkLed(request);
                 return String.valueOf(reply.getLedstate());
             } catch (Exception e) {
@@ -105,11 +121,28 @@ public class HomeActivity extends AppCompatActivity {
                 return;
             }
             if (result.equals("1")) {
-                Button ledOnButton = (Button) activity.findViewById(R.id.ledOnButton);
+                Button ledOnButton = (Button) activity.findViewById(R.id.ledOnButtonRed);
                 ledOnButton.setEnabled(true);
-            } else {
-                Button ledOffButton = (Button) activity.findViewById(R.id.ledOffButton);
+                Button ledOnButtonGreen = (Button) activity.findViewById(R.id.ledOnButtonrGreen);
+                ledOnButtonGreen.setEnabled(true);
+            } else if (result.equals("0")){
+                Button ledOffButton = (Button) activity.findViewById(R.id.ledOffButtonrRed);
                 ledOffButton.setEnabled(true);
+                Button ledOffButtonGreen = (Button) activity.findViewById(R.id.ledOffButtonrGreen);
+                ledOffButtonGreen.setEnabled(true);
+            }
+            else {
+                Toast myToast = Toast.makeText(activity, "You don't have access to this dispositive",
+                        Toast.LENGTH_SHORT);
+                myToast.show();
+                Button ledOnButton = (Button) activity.findViewById(R.id.ledOnButtonRed);
+                ledOnButton.setEnabled(true);
+                Button ledOffButton = (Button) activity.findViewById(R.id.ledOffButtonrRed);
+                ledOffButton.setEnabled(true);
+                Button ledOnButtonGreen = (Button) activity.findViewById(R.id.ledOnButtonrGreen);
+                ledOnButtonGreen.setEnabled(true);
+                Button ledOffButtonGreen = (Button) activity.findViewById(R.id.ledOffButtonrGreen);
+                ledOffButtonGreen.setEnabled(true);
             }
 
         }
