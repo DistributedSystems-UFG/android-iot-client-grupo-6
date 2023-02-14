34310934ccd8818a04b59ffc72833417f7486aeb
Gustavo Marques Reis <gustavo_marquesr@discente.ufg.br>
2023-02-13 23:30:46 -0300

    Deixando a homa adaptativa

--- app/src/main/java/com/example/thermoledmobileclient/HomeActivity.java
+++ app/src/main/java/com/example/thermoledmobileclient/HomeActivity.java
@@ -9,25 +9,27 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.ref.WeakReference;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.examples.iotservice.GetDeviceReply;
+import io.grpc.examples.iotservice.GetDeviceRequest;
 import io.grpc.examples.iotservice.IoTServiceGrpc;
 import io.grpc.examples.iotservice.LedReply;
 import io.grpc.examples.iotservice.LedRequest;
 
 public class HomeActivity extends AppCompatActivity {
-    private Button temperatureButton;
-    private Button ledOnButtonRed;
-    private Button ledOffButtonRed;
-    private Button ledOnButtonGreen;
-    private Button ledOffButtonGreen;
+    private Button temperatureButton, ledOnButtonRed, ledOffButtonRed,
+            ledOnButtonGreen, ledOffButtonGreen;
+    private TextView temperature, ledRed, ledGreen;
     private String host, port, userId;
 
     @Override
@@ -41,11 +43,15 @@ public class HomeActivity extends AppCompatActivity {
         this.port = message[1];
         this.userId = message[2];
 
+        temperature = findViewById(R.id.temperatureTextView);
         temperatureButton = findViewById(R.id.temperatureButton);
+        ledRed = findViewById(R.id.ledRedTextView);
         ledOnButtonRed = findViewById(R.id.ledOnButtonRed);
         ledOffButtonRed = findViewById(R.id.ledOffButtonrRed);
+        ledGreen = findViewById(R.id.ledGreenTextView);
         ledOnButtonGreen = findViewById(R.id.ledOnButtonrGreen);
         ledOffButtonGreen = findViewById(R.id.ledOffButtonrGreen);
+        new UIThread(this, this.host, this.port, this.userId).start();
     }
 
     public void getTemperature(View view) {
@@ -73,6 +79,105 @@ public class HomeActivity extends AppCompatActivity {
     public void ledOffRequestGreen(View view) {
         ledOffButtonRed.setEnabled(false);
         new GrpcTask2(this).execute(this.host, this.port, "led2", this.userId, "0");
+    }
+
+    private class UIThread extends Thread {
+        private Activity activity;
+        private ManagedChannel channel;
+        private String host, portStr, userId;
+        private List<String> userDevices;
+
+        private UIThread(Activity activity, String host, String port, String userId) {
+            this.activity = activity;
+            this.host = host;
+            this.portStr = port;
+            this.userId = userId;
+        }
+
+        @Override
+        public void run() {
+            while (!activity.isDestroyed()) {
+                getUserDevices();
+                changeTempVisibility();
+                changeLedRedVisibility();
+                changeLedGreenVisibility();
+            }
+        }
+
+        private void changeTempVisibility() {
+            int visibility;
+            if (userDevices.contains("tem1")) {
+                visibility = View.VISIBLE;
+            }
+            else {
+                visibility = View.GONE;
+            }
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    temperature.setVisibility(visibility);
+                    temperatureButton.setVisibility(visibility);
+                }
+            });
+        }
+
+        private void changeLedRedVisibility() {
+            int visibility;
+            if (userDevices.contains("led1")) {
+                visibility = View.VISIBLE;
+            }
+            else {
+                visibility = View.GONE;
+            }
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ledRed.setVisibility(visibility);
+                    ledOnButtonRed.setVisibility(visibility);
+                    ledOffButtonRed.setVisibility(visibility);
+                }
+            });
+        }
+
+        private void changeLedGreenVisibility() {
+            int visibility;
+            if (userDevices.contains("led2")) {
+                visibility = View.VISIBLE;
+            }
+            else {
+                visibility = View.GONE;
+            }
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ledGreen.setVisibility(visibility);
+                    ledOnButtonGreen.setVisibility(visibility);
+                    ledOffButtonGreen.setVisibility(visibility);
+                }
+            });
+        }
+
+        private void getUserDevices() {
+            try {
+                int port = TextUtils.isEmpty(portStr) ? 0 : Integer.parseInt(portStr);
+                channel = ManagedChannelBuilder.forAddress(this.host, port).usePlaintext().build();
+                IoTServiceGrpc.IoTServiceBlockingStub stub = IoTServiceGrpc.newBlockingStub(channel);
+                GetDeviceRequest request = GetDeviceRequest.newBuilder().setUserId(this.userId).build();
+                GetDeviceReply reply = stub.getUserDevices(request);
+                this.userDevices = reply.getDeviceIdList();
+            } catch (Exception e) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                pw.flush();
+                this.userDevices = null;
+            }
+            try {
+                channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     private static class GrpcTask2 extends AsyncTask<String, Void, String> {
